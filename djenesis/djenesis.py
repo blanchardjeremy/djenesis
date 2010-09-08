79787da832d891ba7b65113e3fca0359805cccc8
Wiggins <wiggins@concentricsky.com>
2010-09-08 11:14:27 -0700

    cleanup

--- djenesis/djenesis.py
+++ djenesis/djenesis.py
@@ -72,7 +72,6 @@ def my_copytree(src, dst, symlinks=False, ignore=None):
 
 
 def do_copy(template_dir, destination_dir):
-    #print "template: %s\ndestination: %s\n" % (template_dir, destination_dir)
     my_copytree(template_dir, destination_dir, symlinks=True, ignore=shutil.ignore_patterns('.svn*'))
 
     secret_key = generate_secret_key()
@@ -113,7 +112,6 @@ def main():
 
     template_dir = options.template_dir
     if template_dir is None:
-        #template_dir = os.path.join(os.path.abspath(os.path.dirname(__file__).decode('utf-8')), "project_template")
         template_dir = package_data("project_template")
     if not os.path.exists(template_dir):
         print "%s: template directory does not exist!" % (template_dir,)
@@ -151,40 +149,6 @@ def main():
 
     if len(args) > 1:
         pip.main(initial_args=['install']+args[1:])
-
-#    if not options.no_django:
-#        django_url = options.django_url
-#        if django_url is None:
-#            django_url ='http://www.djangoproject.com/download/%s/tarball' % options.django_version
-#
-#        print "Extracting %s..." % django_url
-#
-#        tar_buf = StringIO.StringIO()
-#        uh = urllib2.urlopen(django_url)
-#        tar_buf.write( uh.read() )
-#        uh.close()
-#        tar_buf.seek(0)
-#        tar = tarfile.open(fileobj=tar_buf, mode='r:gz')
-#        for item in tar:
-#            tar.extract(item, path=destination_dir)
-#        tar.close()
-#        uh.close()
-#
-#        symlink_dest = os.path.join(destination_dir,"lib","django")
-#
-#        if hasattr(os, 'symlink'):
-#            os.symlink(os.path.join("..","Django-%s"%options.django_version,"django"),symlink_dest)
-#        else:
-#            print "Symlinks are not supported on your platform. you need to copy %s to %s manually." % (
-#                os.path.join(destination_dir, "Django-%s"%options.django_version, "django"),
-#                os.path.join(destination_dir, "lib", "django"),
-#                )
-#
-    print "Initialized a new djenesis project in %s" % project_dir
-    #print "You now need to copy %s to %s and configure it for your local machine" % (
-        #os.path.join(args[0], "mainsite", "local_settings.py.example"),
-        #os.path.join(args[0], "mainsite", "local_settings.py"),
-        #)
 
 
 if __name__ == '__main__':
