b3e8b2e0a9cf0624c84d527eb3d17b23366096a0
Wiggins <wiggins@concentricsky.com>
2010-09-10 12:05:12 -0700

    fix pip call

--- djenesis/djenesis.py
+++ djenesis/djenesis.py
@@ -3,19 +3,29 @@
 djenesis is a tool for inflating a new Django project
 """
 
-from optparse import OptionParser
+import optparse
 import StringIO
 import os
+import random
 import re
 import shutil
+import subprocess
 import sys
 import tarfile
 import urllib2
-import random
 
 
 def package_data(file_name):
     return os.path.join(os.path.split(__file__)[0], file_name)
+
+
+def call_pip(env_dir, *args):
+    pip_path = os.path.join(env_dir, 'bin', 'pip')
+    try:
+        return subprocess.call(' '.join((pip_path,) + args), shell=True)
+    except OSError, e:
+        print >> sys.stderr, "Execution failed", e
+    return -1
 
 
 def generate_secret_key():
@@ -91,7 +101,7 @@ def do_copy(template_dir, destination_dir):
 
 
 def main():
-    parser = OptionParser(usage="usage: %prog [options] <new-project-directory> [package...]")
+    parser = optparse.OptionParser(usage="usage: %prog [options] <new-project-directory> [package...]")
     parser.add_option("-t", "--template-dir", help="specify a different template directory")
     parser.add_option("-e", "--virtualenv", help="additionally create a virtualenv environment", action="store_true", default=False)
     parser.add_option("", "--env", help="specify the name of the virtualenv directory to create", default="env")
@@ -136,19 +146,14 @@ def main():
 
     if options.requirements or len(args) > 1:
         #activate the virtualenv environment
-        activate_file = os.path.join(env_dir,'bin','activate_this.py')
+        activate_file = os.path.join(env_dir, 'bin', 'activate_this.py')
         execfile(activate_file, dict(__file__=activate_file))
-        try:
-            import pip
-        except ImportError:
-            print "Failed to import pip, is it installed?"
-            sys.exit(1)
 
     if options.requirements:
-        pip.main(initial_args=['install','-r',options.requirements])
+        call_pip(env_dir, 'install', '-r', options.requirements)
 
     if len(args) > 1:
-        pip.main(initial_args=['install']+args[1:])
+        call_pip(env_dir, *(('install',) + tuple(args[1:])))
 
 
 if __name__ == '__main__':
