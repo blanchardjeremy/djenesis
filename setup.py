ac563610b35d4ec870b9a1c28cb9879843ca67d9
Wiggins <wiggins@concentricsky.com>
2010-11-09 14:06:19 -0800

    version bump

--- setup.py
+++ setup.py
@@ -3,13 +3,23 @@
 from distutils.core import setup
 
 setup(name='djenesis',
-    version='0.9.4',
+    version='0.9.5',
     description='Bootstrap django projects using a standard project template',
     author='Concentric Sky',
     author_email='django@concentricsky.com',
     url='http://code.google.com/p/djenesis',
     scripts=['scripts/djenesis'],
     packages=['djenesis'],
-    package_data={'djenesis':
-	['project_template/etc/*', 'project_template/mainsite/*', 'project_template/templates/*']},
+    package_data={'djenesis': [
+        'project_template/apps/*'
+        'project_template/etc/*',
+        'project_template/fixtures/*',
+        'project_template/lib/*',
+        'project_template/mainsite/*',
+        'project_template/media/css/*',
+        'project_template/media/img/*',
+        'project_template/media/js/*',
+        'project_template/media/uploads/*',
+        'project_template/templates/*',
+        ]},
 )
