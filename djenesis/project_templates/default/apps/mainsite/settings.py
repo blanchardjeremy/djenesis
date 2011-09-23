f036e0e83ee6932c1a3d081c8822a3f079ba67a0
Wiggins <wiggins@concentricsky.com>
2011-09-23 14:21:59 -0700

    clean up settings import

--- djenesis/project_templates/default/apps/mainsite/settings.py
+++ djenesis/project_templates/default/apps/mainsite/settings.py
@@ -7,11 +7,96 @@ APPS_DIR = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
 if APPS_DIR not in sys.path:
     sys.path.insert(0, APPS_DIR)
 
-from mainsite import import_settings
+from mainsite import TOP_DIR
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.admin',
+
+    'mainsite',
+)
 
 
-settings_dict = globals()
-# import settings from settings_project and add them to globals
-settings_dict.update(import_settings('mainsite.settings_project'))
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+)
 
-settings_dict.update(import_settings('mainsite.settings_local'))
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.core.context_processors.request',
+    'django.contrib.messages.context_processors.messages',
+)
+
+
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+)
+
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+
+
+MEDIA_ROOT = os.path.join(TOP_DIR, 'media')
+MEDIA_URL = '/media/'
+
+STATIC_ROOT = os.path.join(TOP_DIR, 'static')
+STATIC_URL = '/static/'
+
+ADMIN_MEDIA_PREFIX = '/static/admin/'
+
+ROOT_URLCONF = 'mainsite.urls'
+
+TEMPLATE_DIRS = (
+    os.path.join(TOP_DIR, 'templates'),
+)
+
+SITE_ID = 1
+
+USE_I18N = True
+USE_L10N = True
+
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
+
+
+
+
+# try to import local_settings if present
+try:
+    from local_settings import *
+except ImportError as e:
+    pass
