9c4013491355f535176551cd9184fdf456db6afe
M. Wiggins <code@hlidskialf.com>
2011-09-23 19:10:00 -0700

    refactor template into data_files for distutils

--- setup.py
+++ setup.py
@@ -1,25 +1,26 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
-import djenesis
+import os
+
+
+def walk_data_files(output_path, scan_dir):
+    """Use os.walk() to build a data_files definition for setup()"""
+    data_files = []
+    for dirname, dirs, files in os.walk(scan_dir):
+        files = filter(lambda f: not any([f.startswith('.'), f.endswith('.pyc')]), files)
+        if len(files) > 0:
+            output_dirname = os.path.join(output_path,dirname)
+            files = [os.path.join(dirname, f) for f in files]
+            data_files.append( (output_dirname, files) )
+    return data_files
 
 setup(name='djenesis',
-    version='.'.join(map(str, djenesis.VERSION)),
+    version='0.9.9',
     description='Bootstrap django projects using standard project templates',
     author='Concentric Sky',
     author_email='django@concentricsky.com',
     url='http://github.com/concentricsky/djenesis',
     scripts=['scripts/djenesis'],
-    packages=['djenesis'],
-    package_data={'djenesis': [
-        'project_templates/default/.gitignore',
-        'project_templates/default/apps/mainsite/*',
-        'project_templates/default/requirements.txt',
-        'project_templates/default/media/css/.gitignore',
-        'project_templates/default/media/img/.gitignore',
-        'project_templates/default/media/js/.gitignore',
-        'project_templates/default/media/uploads/.gitignore',
-        'project_templates/default/fixtures/.gitignore',
-        'project_templates/default/templates/*',
-        ]},
+    data_files=walk_data_files('share/djenesis', 'templates'),
 )
